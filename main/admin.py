3576f1854acebc494a895b0aef18e3f1d076f1f9
fadlyatiefn <fadly.atief@ui.ac.id>
2026-09-10 18:30:22 +0700

    feat: integrates the model to the django admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Experience, Projects
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'started_at', 'ended_at', 'is_ongoing']
+    list_filter = ['category']
+    search_fields = ['title', 'description']
+
+@admin.register(Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ['project_name', 'project_start', 'project_end', 'is_ongoing', 'have_project_link']
+    search_fields = ['project_name', 'project_desc']
