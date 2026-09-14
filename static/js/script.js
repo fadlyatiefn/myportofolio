8bfb4aa887793814a7c6605e8f2cd3a26aa99e1f
fadlyatiefn <fadly.atief@ui.ac.id>
2026-09-14 16:26:22 +0700

    feat: redesign for the socials button

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,19 @@
+
+
 const navToggle = document.getElementById('navToggle');
-    const siteNav = document.getElementById('siteNav');
+const siteNav = document.getElementById('siteNav');
+const socialsToggle = document.getElementById('toggle-social');
+const socialsCont = document.getElementById('socials');
 
     function toggleNav() {
         navToggle.classList.toggle('open');
         siteNav.classList.toggle('open');
     }
 
+    function toggleSocials(){
+        socialsCont.classList.toggle('open');
+    }
+
     navToggle.addEventListener('click', toggleNav);
     siteNav.addEventListener('click', toggleNav);
+    socialsToggle.addEventListener('click', toggleSocials);
